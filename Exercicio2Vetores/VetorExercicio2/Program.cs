58ac947a9af56b2c57a51d4825c3f8e2ba50d47b
Mayara Ascari <97797937+AscariMay@users.noreply.github.com>
2022-06-03 11:22:25 -0300

    Add files via upload

---
+++ Exercicio2Vetores/VetorExercicio2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetorExercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe os 20 elementos do Array: ");
+            int[] myArray = new int[20];
+
+            for (int i = 0; i < 20; i++)
+            {
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maior = myArray[0];
+            var posicaoMaior = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                if (myArray[i] > maior)
+                {
+                    maior = myArray[i];
+                    posicaoMaior = i;
+                }
+            }
+            Console.WriteLine($"Maior valor do array: {maior} Posicao: [{posicaoMaior}]");
+
+            int menor = myArray[0];
+            var posicaoMenor = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                if (myArray[i] < menor)
+                {
+                    menor = myArray[i];
+                    posicaoMenor = i;
+                }
+            }
+            Console.WriteLine($"Menor valor do array: {menor} Posicao: [{posicaoMenor}]");
+            Console.ReadLine();
+
+            /*Console.WriteLine("Maior valor do array: " + myArray.Max());
+            Console.WriteLine("Menor valor do array: " + myArray.Min());*/
+        }
+    }
+}
