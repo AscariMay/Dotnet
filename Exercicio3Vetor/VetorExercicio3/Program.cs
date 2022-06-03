69f2c1ef2ae57ddc33e8d7dcd9218fa1fa3782b1
Mayara Ascari <97797937+AscariMay@users.noreply.github.com>
2022-06-03 11:31:19 -0300

    Add files via upload
    Informa a quantidade de números ímpares existentes no vetor.

---
+++ Exercicio3Vetor/VetorExercicio3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetorExercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[100];
+            Console.WriteLine("Informe os 100 elementos: ");
+            Console.ReadLine();
+            int impares = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 100; i++)
+                if (myArray[i] % 2 != 0)
+                    impares++;
+                    Console.WriteLine($"Quantidade de números ímpares: {impares}");
+
+            Console.ReadKey();
+        }
+    }
+}
